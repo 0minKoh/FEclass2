5184c1454c5213748b135d2392da367cef546280
0minKoh <esesang13@naver.com>
2022-04-12 11:22:38 +0900

    badge 애니메이션

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ fadeEls1.forEach(function (fadeEl, index) {
     opacity: 1 // 보이게 함
   })
 })
+
+//.badge 요소를 찾아 badgeEl 변수에 저장
+const badgeEl = document.querySelector('.badge') 
+
+//창(화면)에서 스크롤이 발생하면 함수를 실행하라
+window.addEventListener('scroll', function() {
+//함수: 만약 scroll해서 y좌표가 500을 넘으면 badge를 숨김
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0
+    })
+//스크롤 해서 y가 500을 넘지 않으면 badge를 보여줌
+  } else {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+    })
+  }
+})
